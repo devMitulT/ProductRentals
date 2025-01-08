4baeb57b3da361ed745a537f94032196541ef273
devMitulT <manntari2004@gmail.com>
2025-01-08 17:09:39 +0530

    BE : Intial Commit

---
+++ models/bookingModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = mongoose.Schema(
+  {
+    product_id: {
+      type: Number,
+      required: true,
+      description: 'The ID of the product being booked',
+    },
+    booking_name: {
+      type: String,
+      required: true,
+      description: 'The Name of the user making the booking',
+    },
+    notes: {
+      type: String,
+      description: 'Booking Description ',
+    },
+    start_date: {
+      type: Date,
+      required: true,
+      description: 'The start date of the booking',
+    },
+    end_date: {
+      type: Date,
+      required: true,
+      description: 'The end date of the booking',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;
